092ca4b913014580544312c26e51195f93fd86c5
heleyna <labao.hm@gmail.com>
2015-02-16 18:34:01 +0800

    hello eggs from setup given by Chris

---
+++ appminimal/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='plex',
+    version='0.1.24',
+    packages=find_packages(),
+    url='channelfix.com',
+    license='commercial property',
+    author='Chris Statzer',
+    author_email='chris.statzer@gmail.com',
+    description='The PLEX video funding platform.',
+    install_requires=['django==1.6.8',
+                      'anyjson==0.3.3',
+                      'python-social-auth==0.2.1',
+                      'South==1.0.2',
+                      'celery==3.1.11',
+                      'kombu==3.0.16',
+                      'django-celery==3.1.10',
+                      'Pillow==2.7.0'],
+)
